720b5c484f40928f7611cfa15026b94ee6cfdf3d
SaeGon-Heo <her84562@gmail.com>
2020-01-14 14:06:26 +0900

    Update copyright year

--- app/src/main/java/kr/hsg/clockonscreen/COSMain.java
+++ app/src/main/java/kr/hsg/clockonscreen/COSMain.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2019 SaeGon Heo
+ * Copyright 2020 SaeGon Heo
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
