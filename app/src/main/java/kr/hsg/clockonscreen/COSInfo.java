95a15e89fe2e6e24cdca284dc919f564db885c84
SaeGon-Heo <her84562@gmail.com>
2020-05-03 19:18:11 +0900

    Separate info activity

---
+++ app/src/main/java/kr/hsg/clockonscreen/COSInfo.java
@@ -0,0 +1,190 @@
+/*
+ * Copyright 2020 SaeGon Heo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package kr.hsg.clockonscreen;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.Settings;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public final class COSInfo extends Activity {
+    Context mCon;
+    AlertDialog OpenSourceDialog;
+    Uri GitHubLink;
+
+    // 서비스 실행
+    void restartService() {
+        Intent mSvc_Idle = new Intent(mCon, COSSvc_Idle.class);
+        Intent mSvc = new Intent(mCon, COSSvc.class);
+
+        // 오버레이 권한이 사용자에 의해 풀린 경우 확인하여 서비스를 종료하고 다시 켜도록 유도
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M
+                && !Settings.canDrawOverlays(mCon)) {
+            // 정보 엑티비티 이면 정보 엑티비티만 닫음. service_running 값은 여전히 true 이므로
+            // Main Activity로 복귀하면서 오버레이 권한을 한번더 확인.
+            finish();
+            return;
+        }
+
+        mCon.stopService(mSvc);
+        mCon.stopService(mSvc_Idle);
+        mCon.startService(mSvc_Idle);
+    }
+
+    @Override
+    public void onDestroy() {
+        if(OpenSourceDialog != null && OpenSourceDialog.isShowing()) {
+            OpenSourceDialog.dismiss();
+        }
+
+        super.onDestroy();
+    }
+
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mCon = getApplicationContext();
+
+        setContentView(R.layout.activity_info);
+        setTitle(R.string.action_info);
+
+        // GitHub Uri 미리 생성
+        GitHubLink = Uri.parse("https://github.com/SaeGon-Heo/Clock-On-Screen-android");
+
+        // 버튼 터치 시 효과 (음영)
+        View.OnTouchListener touchEffect = new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+                switch(event.getAction()) {
+                    case MotionEvent.ACTION_UP:
+                    case MotionEvent.ACTION_CANCEL:
+                        view.setBackgroundColor(0xfffafafa);
+                        break;
+                    case MotionEvent.ACTION_DOWN:
+                        view.setBackgroundColor(0xffc0c0c0);
+                        break;
+                }
+                return false;
+            }
+        };
+
+        // 아래 측 TextView를 버튼으로 활용
+        TextView btnGithubLink = findViewById(R.id.btn_github_link);
+        TextView btnOpenSourceLibrary = findViewById(R.id.btn_open_source_library);
+
+        // 버튼 터치 시 효과
+        btnGithubLink.setOnTouchListener(touchEffect);
+        btnOpenSourceLibrary.setOnTouchListener(touchEffect);
+
+        // GitHub Link 버튼 클릭 시
+        // GitHub Repo로 연결
+        btnGithubLink.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(GitHubLink);
+                startActivity(intent);
+            }
+        });
+
+        // Open Source 버튼 클릭 시
+        // 참고한 라이브러리 및 소스 코드를 볼 수 있는 Dialog를 표시
+        btnOpenSourceLibrary.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(OpenSourceDialog == null) {
+                    StringBuilder license = new StringBuilder();
+
+                    /*
+                     * try-with-resources 라는 방식을 쓰면 알아서 close가 된다
+                     * 다만 API 19 이상 지원이여서 쓰지 않음
+                    try (BufferedReader reader = new BufferedReader(new InputStreamReader(con.getInputStream()));)
+                    {
+                        StringBuilder sb = new StringBuilder();
+
+                        String str = null;
+
+                        while (null != (str = reader.readLine()))
+                            sb = sb.append(str);
+
+                        resStr = sb.toString();
+                    }
+                    */
+
+                    InputStreamReader input = null;
+                    BufferedReader reader = null;
+                    try {
+                        input = new InputStreamReader(getAssets().open("opensource.txt"));
+                        reader = new BufferedReader(input);
+
+                        // do reading, usually loop until end of file reading
+                        String mLine = reader.readLine();
+                        while (mLine != null) {
+                            license.append(mLine);
+                            license.append('\n');
+                            mLine = reader.readLine();
+                        }
+                        reader.close();
+                        input.close();
+                    } catch (IOException e) {
+                        try {
+                            if (reader != null) reader.close();
+                        } catch (IOException e1) { }
+                        try {
+                            if (input != null) input.close();
+                        } catch (IOException e2) { }
+
+                        finish();
+                    }
+
+                    OpenSourceDialog = new AlertDialog.Builder(COSInfo.this)
+                            .setPositiveButton(android.R.string.ok, null)
+                            .setIcon(android.R.drawable.ic_menu_help)
+                            .setMessage(license.toString())
+                            .create();
+
+                    license.setLength(0);
+                }
+
+                if(!OpenSourceDialog.isShowing())
+                    OpenSourceDialog.show();
+            }
+        });
+    } // onCreate
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // 서비스가 켜진상태면 재실행
+        if(PreferenceManager.getDefaultSharedPreferences(mCon).getBoolean("service_running", false)) restartService();
+    } // onResume
+}
--- app/src/main/java/kr/hsg/clockonscreen/COSMain.java
+++ app/src/main/java/kr/hsg/clockonscreen/COSMain.java
@@ -38,18 +38,12 @@ import android.widget.TextView;
 import android.content.res.Configuration;
 import android.graphics.Color;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Locale;
 
 public final class COSMain extends Activity {
     Context mCon;
     AlertDialog PermissionDialog;
     AlertDialog AppInfoDialog;
-    AlertDialog OpenSourceDialog;
-    Uri GitHubLink;
-    private boolean bInfo;
 
     // 서비스 실행
     void runService(int way) {
@@ -59,27 +53,20 @@ public final class COSMain extends Activity {
         // 오버레이 권한이 사용자에 의해 풀린 경우 확인하여 서비스를 종료하고 다시 켜도록 유도
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M
                 && !Settings.canDrawOverlays(mCon)) {
-            // 정보 엑티비티 이면 정보 엑티비티만 닫음. service_running 값은 여전히 true 이므로
-            // 이곳으로 다시 들어와서 오버레이 권한을 한번더 확인.
-            if(bInfo) {
-                finish();
-            }
-            // 메인 엑티비티 이면 service_running 값을 false 으로 저장 및 서비스 중지 상태로 버튼을 전환
-            else {
-                // 서비스 종료
-                mCon.stopService(mSvc); mCon.stopService(mSvc_Idle);
+            // service_running 값을 false 으로 저장 및 서비스 중지 상태로 버튼을 전환
+            mCon.stopService(mSvc); mCon.stopService(mSvc_Idle);
 
-                // 서비스 실행 상태 꺼짐으로 변경 및 서비스 켜기/끄기 버튼 상태를 끄기로 변경
-                PreferenceManager.getDefaultSharedPreferences(mCon).edit().putBoolean("service_running", false).apply();
+            // 서비스 실행 상태 꺼짐으로 변경 및 서비스 켜기/끄기 버튼 상태를 끄기로 변경
+            PreferenceManager.getDefaultSharedPreferences(mCon).edit().putBoolean("service_running", false).apply();
 
-                ((ImageView)findViewById(R.id.btn_svc_toggle)).setImageResource(R.mipmap.ic_svc_off);
-                TextView tv = findViewById(R.id.tv_svc_state);
-                tv.setText(R.string.text_svc_off);
-                tv.setTextColor(Color.RED);
+            ((ImageView)findViewById(R.id.btn_svc_toggle)).setImageResource(R.mipmap.ic_svc_off);
+            TextView tv = findViewById(R.id.tv_svc_state);
+            tv.setText(R.string.text_svc_off);
+            tv.setTextColor(Color.RED);
 
-                // 다시 오버레이 권한을 켜도록 유도
-                induceSetOverlayPermission();
-            }
+            // 다시 오버레이 권한을 켜도록 유도
+            induceSetOverlayPermission();
+
             return;
         }
 
@@ -117,19 +104,12 @@ public final class COSMain extends Activity {
 
     @Override
     public void onDestroy() {
-        if(bInfo) {
-            if(OpenSourceDialog != null && OpenSourceDialog.isShowing()) {
-                OpenSourceDialog.dismiss();
-            }
-        } // Info Activity
-        else {
-            if (PermissionDialog != null && PermissionDialog.isShowing()) {
-                PermissionDialog.dismiss();
-            }
-            if (AppInfoDialog != null && AppInfoDialog.isShowing()) {
-                AppInfoDialog.dismiss();
-            }
-        } // Main Activity
+        if (PermissionDialog != null && PermissionDialog.isShowing()) {
+            PermissionDialog.dismiss();
+        }
+        if (AppInfoDialog != null && AppInfoDialog.isShowing()) {
+            AppInfoDialog.dismiss();
+        }
         super.onDestroy();
     }
 
@@ -200,195 +180,90 @@ public final class COSMain extends Activity {
         super.onCreate(savedInstanceState);
 
         mCon = getApplicationContext();
-        bInfo = getIntent().getBooleanExtra("Info", false);
 
-        if(bInfo) {
-            setContentView(R.layout.activity_info);
-            setTitle(R.string.action_info);
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(mCon);
 
-            // GitHub Uri 미리 생성
-            GitHubLink = Uri.parse("https://github.com/SaeGon-Heo/Clock-On-Screen-android");
+        // 현재 Context의 언어 설정
+        Locale locale;
+        String currentLocale = Locale.getDefault().toString();
+        if (pref.getBoolean(mCon.getString(R.string.pref_english_key_string), !(currentLocale.equals("ko_KR") || currentLocale.equals("ko"))))
+            locale = Locale.US;
+        else
+            locale = Locale.KOREA;
 
-            // 버튼 터치 시 효과 (음영)
-            View.OnTouchListener touchEffect = new View.OnTouchListener() {
-                @Override
-                public boolean onTouch(View view, MotionEvent event) {
-                    switch(event.getAction()) {
-                        case MotionEvent.ACTION_UP:
-                            view.setBackgroundColor(0xfffafafa);
-                            break;
-                        case MotionEvent.ACTION_DOWN:
-                            view.setBackgroundColor(0xffc0c0c0);
-                            break;
-                        case MotionEvent.ACTION_CANCEL:
-                            view.setBackgroundColor(0xfffafafa);
-                            break;
-                    }
-                    return false;
+        Configuration config = new Configuration();
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            config.setLocale(locale);
+        } else {
+            config.locale = locale;
+        }
+        getResources().updateConfiguration(config, getResources().getDisplayMetrics());
+
+        // 메인화면 출력
+        setContentView(R.layout.activity_main);
+
+        // 오버레이 권한 확인 및 오버레이 권한이 풀린 경우 다시 켜도록 유도
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M
+                && !Settings.canDrawOverlays(mCon))
+            induceSetOverlayPermission();
+
+        // 첫실행 또는 설정 초기화 실행 후 초기 값을 설정
+        if (pref.getBoolean("init_settings", true))
+            initSettings();
+
+        // 서비스 상태에 따라 글자색 및 이미지를 바꾸기 위해 선언
+        TextView tv = findViewById(R.id.tv_svc_state);
+        ImageView iv = findViewById(R.id.btn_svc_toggle);
+
+        // 서비스 실행상태시 안내문 및 아이콘 변경
+        if (pref.getBoolean("service_running", false)) {
+            iv.setImageResource(R.mipmap.ic_svc_on);
+            tv.setText(R.string.text_svc_on);
+            tv.setTextColor(Color.GREEN);
+        }
+
+        // 시작 / 중지버튼
+        iv.setOnClickListener(new View.OnClickListener() {
+            // 설정값에 따라 서비스를 시작 / 중지
+            @Override
+            public void onClick(View v) {
+                SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+                TextView tv = findViewById(R.id.tv_svc_state);
+                ImageView iv = findViewById(R.id.btn_svc_toggle);
+
+                if (pref.getBoolean("service_running", false)) {
+                    // 서비스 상태에 따라 글자색 및 이미지를 바꾸기 위해 선언
+                    iv.setImageResource(R.mipmap.ic_svc_off);
+                    tv.setText(R.string.text_svc_off);
+                    tv.setTextColor(Color.RED);
+                    pref.edit().putBoolean("service_running", false).apply();
+                    runService(0);
+                } else {
+                    iv.setImageResource(R.mipmap.ic_svc_on);
+                    tv.setText(R.string.text_svc_on);
+                    tv.setTextColor(Color.GREEN);
+                    pref.edit().putBoolean("service_running", true).apply();
+                    runService(1);
                 }
-            };
-
-            // 아래 측 TextView를 버튼으로 활용
-            TextView btn_github_link = findViewById(R.id.btn_github_link);
-            TextView btn_open_source_library = findViewById(R.id.btn_open_source_library);
-
-            // 버튼 터치 시 효과
-            btn_github_link.setOnTouchListener(touchEffect);
-            btn_open_source_library.setOnTouchListener(touchEffect);
-
-            // GitHub Link 버튼 클릭 시
-            // GitHub Repo로 연결
-            btn_github_link.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View v) {
-                    Intent intent = new Intent();
-                    intent.setAction(Intent.ACTION_VIEW);
-                    intent.addCategory(Intent.CATEGORY_BROWSABLE);
-                    intent.setData(GitHubLink);
-                    startActivity(intent);
-                }
-            });
-
-            // Open Source 버튼 클릭 시
-            // 참고한 라이브러리 및 소스 코드를 볼 수 있는 Dialog를 표시
-            btn_open_source_library.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    if(OpenSourceDialog == null) {
-                        StringBuilder license = new StringBuilder();
-
-                        /*
-                         * try-with-resources 라는 방식을 쓰면 알아서 close가 된다
-                         * 다만 API 19 이상 지원이여서 쓰지 않음
-                        try (BufferedReader reader = new BufferedReader(new InputStreamReader(con.getInputStream()));)
-                        {
-                            StringBuilder sb = new StringBuilder();
-
-                            String str = null;
-
-                            while (null != (str = reader.readLine()))
-                                sb = sb.append(str);
-
-                            resStr = sb.toString();
-                        }
-                        */
-
-                        try {
-                            InputStreamReader input = new InputStreamReader(getAssets().open("opensource.txt"));
-                            BufferedReader reader = new BufferedReader(input);
-
-                            // do reading, usually loop until end of file reading
-                            String mLine = reader.readLine();
-                            while (mLine != null) {
-                                license.append(mLine);
-                                license.append('\n');
-                                mLine = reader.readLine();
-                            }
-                            reader.close();
-                            input.close();
-                        } catch (IOException e) {
-                            finish();
-                        }
-
-                        OpenSourceDialog = new AlertDialog.Builder(COSMain.this)
-                                .setPositiveButton(android.R.string.ok, null)
-                                .setIcon(android.R.drawable.ic_menu_help)
-                                .setMessage(license.toString())
-                                .create();
-
-                        license.setLength(0);
-                    }
-
-                    if(!OpenSourceDialog.isShowing())
-                        OpenSourceDialog.show();
-                }
-            });
-        } // Info Activity
-        else {
-            SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(mCon);
-
-            // 현재 Context의 언어 설정
-            Locale locale;
-            String currentLocale = Locale.getDefault().toString();
-            if (pref.getBoolean(mCon.getString(R.string.pref_english_key_string), !(currentLocale.equals("ko_KR") || currentLocale.equals("ko"))))
-                locale = Locale.US;
-            else
-                locale = Locale.KOREA;
-
-            Configuration config = new Configuration();
-            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN_MR1) {
-                config.setLocale(locale);
-            } else {
-                config.locale = locale;
             }
-            getResources().updateConfiguration(config, getResources().getDisplayMetrics());
+        });
 
-            // 메인화면 출력
-            setContentView(R.layout.activity_main);
-
-            // 오버레이 권한 확인 및 오버레이 권한이 풀린 경우 다시 켜도록 유도
-            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M
-                    && !Settings.canDrawOverlays(mCon))
-                induceSetOverlayPermission();
-
-            // 첫실행 또는 설정 초기화 실행 후 초기 값을 설정
-            if (pref.getBoolean("init_settings", true))
-                initSettings();
-
-            // 서비스 상태에 따라 글자색 및 이미지를 바꾸기 위해 선언
-            TextView tv = findViewById(R.id.tv_svc_state);
-            ImageView iv = findViewById(R.id.btn_svc_toggle);
-
-            // 서비스 실행상태시 안내문 및 아이콘 변경
-            if (pref.getBoolean("service_running", false)) {
-                iv.setImageResource(R.mipmap.ic_svc_on);
-                tv.setText(R.string.text_svc_on);
-                tv.setTextColor(Color.GREEN);
+        // 버튼 터치 시 효과
+        iv.setOnTouchListener(new OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_UP:
+                    case MotionEvent.ACTION_CANCEL:
+                        view.setBackgroundColor(0xfffafafa);
+                        break;
+                    case MotionEvent.ACTION_DOWN:
+                        view.setBackgroundResource(R.mipmap.ic_svc_background);
+                        break;
+                }
+                return false;
             }
-
-            // 시작 / 중지버튼
-            iv.setOnClickListener(new View.OnClickListener() {
-                // 설정값에 따라 서비스를 시작 / 중지
-                @Override
-                public void onClick(View v) {
-                    SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-                    TextView tv = findViewById(R.id.tv_svc_state);
-                    ImageView iv = findViewById(R.id.btn_svc_toggle);
-
-                    if (pref.getBoolean("service_running", false)) {
-                        // 서비스 상태에 따라 글자색 및 이미지를 바꾸기 위해 선언
-                        iv.setImageResource(R.mipmap.ic_svc_off);
-                        tv.setText(R.string.text_svc_off);
-                        tv.setTextColor(Color.RED);
-                        pref.edit().putBoolean("service_running", false).apply();
-                        runService(0);
-                    } else {
-                        iv.setImageResource(R.mipmap.ic_svc_on);
-                        tv.setText(R.string.text_svc_on);
-                        tv.setTextColor(Color.GREEN);
-                        pref.edit().putBoolean("service_running", true).apply();
-                        runService(1);
-                    }
-                }
-            });
-
-            // 버튼 터치 시 효과
-            iv.setOnTouchListener(new OnTouchListener() {
-                @Override
-                public boolean onTouch(View view, MotionEvent event) {
-                    switch (event.getAction()) {
-                        case MotionEvent.ACTION_UP:
-                            view.setBackgroundColor(0xfffafafa);
-                            break;
-                        case MotionEvent.ACTION_DOWN:
-                            view.setBackgroundResource(R.mipmap.ic_svc_background);
-                            break;
-                        case MotionEvent.ACTION_CANCEL:
-                            view.setBackgroundColor(0xfffafafa);
-                    }
-                    return false;
-                }
-            });
-        } // Main Activity
+        });
     } // onCreate
 
     @Override
@@ -401,8 +276,7 @@ public final class COSMain extends Activity {
     // 메인 화면에만 메뉴를 로드
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        if(!bInfo)
-            getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
@@ -414,7 +288,7 @@ public final class COSMain extends Activity {
                 startActivity(new Intent(this, COSSettings.class));
                 return true;
             case R.id.action_info:
-                startActivity(new Intent(this, COSMain.class).putExtra("Info", true));
+                startActivity(new Intent(this, COSInfo.class));
                 return true;
             case R.id.action_exit:
                 if(!isFinishing()) finish();
